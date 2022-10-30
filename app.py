a7f384bfefcaf211a3b60ff523b2072c5e676bac
alejomart <alejomart789@gmail.com>
2022-10-30 15:51:35 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, make_response, redirect, render_template
+
+app = Flask(__name__)
+
+todos = ['Comprar cafe', 'Solicitud de compra', 'Entregar video productor']
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'todos': todos,
+        'user_ip': user_ip,
+    }
+
+    return render_template('hello.html', **context)
